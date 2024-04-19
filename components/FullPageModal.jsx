7c286ca92476ea07e98f9c4fedf670bcdce83b29
phianova <98988580+phianova@users.noreply.github.com>
2024-04-19 16:23:58 +0100

    fix close button

--- components/FullPageModal.jsx
+++ components/FullPageModal.jsx
@@ -14,9 +14,14 @@ const FullPageModal = (props) => {
             <button onClick={handleClick} className="text-3xl p-4 my-3 border-2 rounded-lg border-colour-text hover:bg-colour-text hover:text-colour-bg">View graph: {graphTitle}</button>
             {isOpen && dataSource &&
                 <div className="fixed top-10 left-5 sm:top-20 sm:left-10 sm:right-10 w-11/12 h-5/6 sm:h-3/4 overflow-scroll z-6 bg-white border-2 border-colour-text">
-                    <div className="fixed mt-5 mx-auto w-11/12 text-center flex flex-row justify-center items-center p-5">Scroll to view graph<FaArrowsAlt className="m-3"/></div>
+                    <div className="flex flex-row w-full justify-end">
+                    <div className="fixed mt-5 mx-auto w-11/12 text-center flex flex-row justify-center items-center p-5">
+                        <p className="">Scroll to view graph</p>
+                        <FaArrowsAlt className="m-3" />
+                    </div>
+                    <button className="fixed font-bold hover:text-colour-link p-3" onClick={handleClick}>Close</button>
+                    </div>
                     <object type="image/svg+xml" data={dataSource} className="p-10"></object>
-                    <button className="fixed top-12 right-8 sm:top-24 sm:right-20 font-bold hover:text-colour-link" onClick={handleClick}>Close</button>
                 </div>
             }
         </div>
